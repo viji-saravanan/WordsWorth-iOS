af1b95ec210e5460d2e41c3842b35ea2d467bc70
callmearya <arcx.dj@gmail.com>
2025-12-28 10:18:35 +0530

    Initial Commit

---
+++ word game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  word game
+//
+//  Created by Arya Subramani on 28/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ word game/word_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  word_gameApp.swift
+//  word game
+//
+//  Created by Arya Subramani on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct word_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
